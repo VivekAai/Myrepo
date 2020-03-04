69381ea7de5e104af4b10580630e657df673134a
VivekAai <61734194+VivekAai@users.noreply.github.com>
2020-03-04 11:53:34 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		int a[]=new int ['a'];
+		System.out.println(a);//
+        System.out.println(a.length); 
+        
+	}
+
+}
